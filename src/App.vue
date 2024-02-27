685adc3b3c1386917b7dd6e0bd5bae3b3b64b116
kahhong01 <kahhong@newswav.com>
2024-02-27 22:15:15 +0800

    made homepage layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import AppHeader from './components/AppHeader.vue'
 </script>
+
 <template>
+  <AppHeader />
   <RouterView />
 </template>
 
 <style scoped>
 </style>
+
+
+
+
+
