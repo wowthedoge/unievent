43f434b2d299c21e3f7f5d12aa1f15bd723936cd
kahhong01 <kahhong@newswav.com>
2024-02-27 15:14:55 +0800

    init firebase

--- src/main.js
+++ src/main.js
@@ -4,6 +4,23 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_APP_FIREBASE_MEASUREMENT_ID
+}
+
+// Initialize Firebase
+initializeApp(firebaseConfig)
+
 const app = createApp(App)
 
 app.use(router)
