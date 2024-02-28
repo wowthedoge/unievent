e91f1efc9ff95efe1c977ac1f6f95a038e37bcf2
khailoong-nw <khailoong@newswav.com>
2024-02-28 12:56:02 +0800

    Login Page

---
+++ src/views/LoginView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div ref="firebaseui" class="firebaseui-container"></div>
+  </div>
+</template>
+
+<script>
+import { getAuth } from 'firebase/auth';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import * as firebaseui from 'firebaseui';
+
+export default {
+  mounted() {
+    const uiConfig = {
+      signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      ],
+      signInSuccessUrl: '/',
+      callbacks: {
+        signInSuccessWithAuthResult: () => false,
+      },
+    };
+
+    const auth = getAuth();
+    const ui = new firebaseui.auth.AuthUI(auth);
+    ui.start(this.$refs.firebaseui, uiConfig);
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
+
+
